7f4054944b201a6d5ae8ba334762dc4d2db37303
Pere Balsach <pere.balsach@king.com>
2019-05-16 14:29:08 +0200

    Added character

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocity;
+
+    private Rigidbody rb;
+    
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        
+        
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+
+        Debug.Log(movement);
+        rb.velocity = movement * vertical;
+        
+    }
+}
