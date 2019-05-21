dc8cf32d2b65e97407d261c94fa8339e8f0407d7
Unknown <unknown@example.com>
2019-05-21 13:28:11 +0200

    Movement and Simple menu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float velocity;
-
+    public float dashForce;
     private Rigidbody rb;
     
     private void Start()
@@ -19,12 +19,23 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         
-        
-        
         Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
 
-        Debug.Log(movement);
-        rb.velocity = movement * vertical;
-        
+        if (movement != Vector3.zero)
+        {
+            this.transform.forward = movement;
+        }
+
+        rb.MovePosition(rb.position + movement * velocity * Time.fixedDeltaTime);
+
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Dash");
+
+            Vector3 dashVelocity = Vector3.Scale(transform.forward, dashForce * new Vector3((Mathf.Log(1f / (Time.deltaTime * rb.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * rb.drag + 1)) / -Time.deltaTime)));
+            rb.AddForce(dashVelocity, ForceMode.VelocityChange);
+        }
+
+
     }
 }
