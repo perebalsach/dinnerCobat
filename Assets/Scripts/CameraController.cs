b956ecb0183910563a9d1c46f6bbc3a4978a414c
neusperez <neus.perez@king.com>
2019-06-03 13:36:30 +0200

    Dash and almost the camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject target;
+    public float rotationSpeed = 3f;
+    private Vector3 distance;
+    float desiredAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = target.transform.position - transform.position;
+        desiredAngle = target.transform.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * rotationSpeed;
+        //float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        
+
+        
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        transform.position = target.transform.position - (rotation * distance);
+
+        transform.LookAt(target.transform);
+
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log("Right Click");
+            desiredAngle = target.transform.eulerAngles.y;
+            target.transform.Rotate(0, horizontal, 0);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float velocity;
+    public float speed;
     public float tolerancia;
+
+    public float DashDistance = 5f;
+
+    public const float maxDashTime = 100.0f;
+    public float dashStoppingSpeed = 0.001f;
+    public float dashSpeed = 100;
 
     private Rigidbody _rb;
     private Animator _animator;
@@ -13,6 +19,8 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         _rb = this.GetComponent<Rigidbody>();
+        _rb.drag = 15;
+    
         _animator = this.GetComponent<Animator>();
     }
 
@@ -21,18 +29,20 @@ public class PlayerController : MonoBehaviour
 
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        
-        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical).normalized; ;
 
         if (movement != Vector3.zero)
         {
             this.transform.forward = movement;
         }
+
+        //_rb.velocity = movement * speed;
+        _rb.MovePosition(_rb.position + movement * speed * Time.fixedDeltaTime);
+
+
         
-        _rb.velocity = new Vector3(movement.x, movement.y, movement.z) * velocity;
-
-
-        if (Mathf.Abs(_rb.velocity.x) > tolerancia || Mathf.Abs(_rb.velocity.y) > tolerancia || Mathf.Abs(_rb.velocity.z) > tolerancia)
+        if (Mathf.Abs(horizontal) > tolerancia || Mathf.Abs(vertical) > tolerancia)
         {
             _animator.SetBool("move", true);
         }
@@ -40,8 +50,29 @@ public class PlayerController : MonoBehaviour
         {
             _animator.SetBool("move", false);
         }
-        
 
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * _rb.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * _rb.drag + 1)) / -Time.deltaTime)));
+            _rb.AddForce(dashVelocity, ForceMode.VelocityChange);
+        }
     }
+
+
+    /*
+    IEnumerator Dash()
+    {
+        float dashTime = 0;
+       
+        _rb.velocity = this.transform.forward * dashSpeed;
+        while (dashTime < maxDashTime)
+        {
+            dashTime += Time.deltaTime;
+            yield return new WaitForSeconds(0.01f);
+            //_rb.velocity = this.transform.forward * dashSpeed;
+        }
+        _rb.AddForce(0,0,0);
+        
+        //_rb.velocity = this.transform.forward * dashSpeed;
+    }*/
 }
