adc6d0b073a061b498ce9e40211f4708a5093514
neusperez <neus.perez@king.com>
2019-06-06 14:03:00 +0200

    Camera and first version of changing target

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,15 +6,20 @@ public class CameraController : MonoBehaviour
 {
 
     public GameObject target;
+    public GameObject bossTaregt;
     public float rotationSpeed = 3f;
     private Vector3 distance;
     float desiredAngle;
 
+    private GameObject viewTarget;
+    private bool pointBoss = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        viewTarget = target;
         distance = target.transform.position - transform.position;
-        desiredAngle = target.transform.eulerAngles.y;
+        desiredAngle = target.transform.localEulerAngles.y;
     }
 
     // Update is called once per frame
@@ -22,19 +27,39 @@ public class CameraController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Mouse X") * rotationSpeed;
         //float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
-        
 
-        
+
+
         Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
         transform.position = target.transform.position - (rotation * distance);
 
-        transform.LookAt(target.transform);
+        transform.LookAt(viewTarget.transform);
 
         if (Input.GetMouseButton(1))
         {
-            Debug.Log("Right Click");
-            desiredAngle = target.transform.eulerAngles.y;
+            desiredAngle = target.transform.localEulerAngles.y;
             target.transform.Rotate(0, horizontal, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            desiredAngle = target.transform.localEulerAngles.y;
+            target.transform.Rotate(0, horizontal, 0);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("Target BOSS!");
+            if (!pointBoss)
+            {
+                viewTarget = bossTaregt;
+            }
+            else
+            {
+                viewTarget = target;
+            }
+            pointBoss = !pointBoss;            
         }
     }
 }
