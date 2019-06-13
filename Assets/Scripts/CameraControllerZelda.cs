3e00b487f2f4fc16b725ba35021a16b5dbcc225d
Pere Balsach <pere.balsach@king.com>
2019-06-13 14:17:06 +0200

    added camera test scene for the new camera controller

---
+++ Assets/Scripts/CameraControllerZelda.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerZelda : MonoBehaviour
+{
+
+    [SerializeField] private float distanceAway;
+    [SerializeField] private float distanceUp;
+    [SerializeField] private float smooth;
+    [SerializeField] private Transform follow;
+    private Vector3 _targetPosition;
+    
+    
+    void Start()
+    {
+        follow = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        _targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
+
+        transform.position = Vector3.Lerp(transform.position, _targetPosition, Time.deltaTime * smooth);
+        transform.LookAt(follow);
+    }
+}
