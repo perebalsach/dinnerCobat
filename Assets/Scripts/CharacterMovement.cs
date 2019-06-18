cd931e6249e13dbf24d2bdda34685a7733370e60
Pere Balsach <pere.balsach@gmail.com>
2019-06-18 22:59:12 +0200

    Added character movement controller and a working version of the character camera and movement. Added new animations for the bot + rig

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Animator _animator;
+    private float _horizontal;
+    private float _vertical;
+
+    void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+         _horizontal = Input.GetAxis("Horizontal");
+        _animator.SetFloat("direction", _horizontal);
+
+        _vertical = Input.GetAxis("Vertical");          
+        _animator.SetFloat("speed", _vertical);
+
+    }
+}
