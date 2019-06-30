5fa2672cc222fb5b39e0bdd5f66e8d2561bd36b3
Pere Balsach <pere.balsach@gmail.com>
2019-06-30 23:17:14 +0200

    Added new player movement script

---
+++ Assets/PlayerMotion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotion : MonoBehaviour
+{
+
+    public float moveSpeed = 10f;
+    public float rotationSpeed = 75f;
+
+    private float _vInput;
+    private float _hInput;
+
+    void Update()
+    {
+        _hInput = Input.GetAxis("Horizontal") * rotationSpeed;
+        _vInput = Input.GetAxis("Vertical") * moveSpeed;
+        
+        this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
+        this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
+    }
+}
