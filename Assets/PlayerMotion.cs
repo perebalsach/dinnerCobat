283ca0227d9c6f752f16549efc4bcb123f1880e0
Pere Balsach <pere.balsach@king.com>
2019-07-02 16:42:44 +0200

    Added camera and character movement

--- Assets/PlayerMotion.cs
+++ Assets/PlayerMotion.cs
@@ -7,16 +7,28 @@ public class PlayerMotion : MonoBehaviour
 
     public float moveSpeed = 10f;
     public float rotationSpeed = 75f;
+    public Camera cam;
 
     private float _vInput;
     private float _hInput;
+    private Animator animator;
 
-    void Update()
+    private void Start() 
     {
+        animator = this.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        var p = cam.transform.forward + this.transform.position;
+        
         _hInput = Input.GetAxis("Horizontal") * rotationSpeed;
         _vInput = Input.GetAxis("Vertical") * moveSpeed;
-        
+
+        animator.SetFloat("direction", _vInput);
+
         this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
-        this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
+        this.transform.Rotate(Vector3.up * (_hInput + p.y) * Time.deltaTime);
+
     }
 }
---
+++ Assets/Scripts/CameraMotion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotion : MonoBehaviour
+{
+    public GameObject Player;
+    public float YOffset;
+    public float ZOffset;
+    public float OffsetTargetY;
+
+
+    void Update()
+    {
+        this.transform.position = new Vector3(Player.transform.position.x, 
+                                                Player.transform.position.y + YOffset,
+                                                Player.transform.position.z + ZOffset
+                                            );
+
+        var lookAt = new Vector3(Player.transform.position.x, 
+                                    Player.transform.position.y + OffsetTargetY,
+                                    Player.transform.position.z
+                                );
+
+        this.transform.LookAt(lookAt);
+    }
+}
